c199a4f48cad4bff7e406a45b374aa409d1f5e06
Daniel Khayat <daniel.khayat8@gmail.com>
2023-12-13 22:35:23 +0200

    #LeetCode .121 - Best Time to buy and sell stock

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ from solutions import Solutions
 
 def main():
     test = Solutions()
-    array1 = [1,2,3,4,5,67,8,9]
-    print(test.twoSum(array1, 68))
+    array1 = [1, 2, 3, 4, 5, 67, 8, 9]
+    array2 = [6, 7, 2, 9, 6]
+    print(test.twoSum(array1, 68) == [0,5])
+    print(test.buySellStocks(array2) == 7)
 
 if __name__ == "__main__":
     main()
--- solutions.py
+++ solutions.py
@@ -9,3 +9,17 @@ class Solutions:
                 return [hashMap[diff], i]
             hashMap[num] = i
         return
+
+    #LeetCode .121 - Best Time to buy and sell stock
+    def buySellStocks(self, arr):
+        buyDay = 0 # At the beggining we must initiate the firsst two day at 0 and 1.
+        sellDay = 1
+        tmpMax = -1 # Initiate a temporary maximum value to iterate over the array.
+        while sellDay < len(arr) :
+            if(arr[buyDay] < arr[sellDay]):
+                profit = arr[sellDay] - arr[buyDay]
+                tmpMax = profit if profit > tmpMax else tmpMax
+            else :
+                buyDay = sellDay
+            sellDay += 1
+        return tmpMax           
